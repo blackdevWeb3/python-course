2eeb80fb4e26fdac7c4fe97f51c0f91ba7082a46
blackdevWeb3 <tan.haruki93@gmail.com>
2025-05-16 20:35:04 -0500

    final

--- app.py
+++ app.py
@@ -13,3 +13,35 @@ print(course_name[0:3])
 print(course_name[0:])
 print(course_name[:3])
 print(course_name[:])
+
+# string methods
+first = 'John'
+last = 'Smith'
+full_name = f"{first} {last}"
+print(full_name.upper())
+
+# # while loop
+# command = ""
+# while command != 'quit':
+#     command = input(">")
+#     print("ECHO", command)
+
+# for loop
+for number in range(1, 10):
+    if number % 2 == 0:
+        print(number)
+
+# functions
+
+
+# def greet_user(firstname, lastname):
+#     print(f"Hi {firstname}, {lastname}")
+#     print("Welcome aboard!")
+
+
+def get_greeting(name):
+    return f"Hi {name}"
+
+
+message = get_greeting("John")
+print(message)
